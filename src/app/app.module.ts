d82c0400a09adbc96739b8a2867aca6019cf7c57
ESP\Sabin.Iliescu <sabin-adrian.iliescu@oath.com>
2019-02-01 14:14:58 +0200

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component'; // <-- NgModel lives here
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { MessagesComponent } from './messages/messages.component'; // <-- NgMode
         AppComponent,
         HeroesComponent,
         HeroDetailComponent,
-        MessagesComponent
+        MessagesComponent,
+        DashboardComponent
     ],
     imports: [
         BrowserModule,
